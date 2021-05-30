916eeaadfa269fcb1aee170d39592edef236a6af
Diandra <leao.diandra@gmail.com>
2021-05-30 18:30:57 -0300

    add chamada de news

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -11,6 +11,7 @@ import scss from '../css/main.scss';
  * Components
  */
 import cards from './components/cards.js';
+import news from './components/news.js';
 
 
 if (document.body.classList.contains('home-page')) {
@@ -19,6 +20,11 @@ if (document.body.classList.contains('home-page')) {
    * cards
    */
   cards.buildCards();
-  
+ 
+  /**
+   * news
+   */ 
+  news.isValid();
+  news.addNewEmail();
 
 }
